b59c92aacc0b4801a1d3df670f487b2abb6bb98e
FabricioLima-C <fabriciocabral07@gmail.com>
2020-05-12 22:32:20 -0300

    First commit

---
+++ src/main/java/com/example/demo/CourseMongodbSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMongodbSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMongodbSpringBootApplication.class, args);
+	}
+
+}
