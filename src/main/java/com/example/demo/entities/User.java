c2e24a8e11cad8d88bfe38ec9219f3981f4d4b27
FabricioLima-C <fabriciocabral07@gmail.com>
2020-05-12 23:34:50 -0300

    Connect on mongodb

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -2,10 +2,15 @@ package com.example.demo.entities;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private Long id;
 	private String name;
 	private String email;
--- src/main/java/com/example/demo/resource/UserResource.java
+++ src/main/java/com/example/demo/resource/UserResource.java
@@ -1,21 +1,27 @@
 package com.example.demo.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
 	// O ResponseEntity é um objeto que encapsula respostas http com cabeçalhos, erros dentre outros
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<User> findAll() {
-		
-		return ResponseEntity.ok().body(null);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
