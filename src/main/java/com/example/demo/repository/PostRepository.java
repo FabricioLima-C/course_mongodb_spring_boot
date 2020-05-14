4401aae9877988aa390bd1f7dfba0d4e1bba9fc5
FabricioLima-C <fabriciocabral07@gmail.com>
2020-05-13 22:24:12 -0300

    Simple consulting with @Query

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entities.Post;
@@ -14,4 +15,9 @@ public interface PostRepository extends MongoRepository<Post, Long>{
 	// O IgnoreCase ignora letras maiúsculas e minúsculas, o framework tem essas funções prontas
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
+	// Query personalizada
+	// ?0 pega o valor passado pelo parâmetro
+	// Mais informações sobre a personalização das queries, https://docs.mongodb.com/manual/reference/operator/query/regex/
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 }
