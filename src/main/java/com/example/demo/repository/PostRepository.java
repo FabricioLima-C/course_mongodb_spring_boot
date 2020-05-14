00d6bb42f32841e593b2ddf06846a7435eb3e6b2
FabricioLima-C <fabriciocabral07@gmail.com>
2020-05-13 22:13:19 -0300

    Simple queries with query method

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.example.demo.entities.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, Long>{
-
+	
+	// O framework cria o método para consulta no banco de dados
+	// O IgnoreCase ignora letras maiúsculas e minúsculas, o framework tem essas funções prontas
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.example.demo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.Post;
-import com.example.demo.entities.User;
 import com.example.demo.repository.PostRepository;
 import com.example.demo.services.exceptions.ObjectNotFoundException;
 
@@ -19,5 +19,9 @@ public class PostService {
 	public Post findById(Long id) {
 		Optional<Post> post = repository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Not found object"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repository.findByTitleContaining(text);
 	}
 }
